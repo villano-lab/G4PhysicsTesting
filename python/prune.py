e36a72371ec96c4aef1beb911f9d2dea1a67353f
Kitty <kathryn.harris@ucdenver.edu>
2022-01-29 19:48:59 -0700

    Set up many necessary files for plotting results from output.

--- python/prune.py
+++ python/prune.py
@@ -19,13 +19,12 @@ infilecontents = pd.DataFrame()
 # Go through all the files.
 for arg in sys.argv:
     if (arg != __file__ and arg != os.path.basename(__file__) and arg is not sys.argv[-1]): #Don't include the running of the script as an argument.
-        infilenames.append(arg)                                 #note all our filenames
-        infilecontents = pd.concat(                             #Combine all the data, but only the parts we need.
+        infilenames.append(arg)                                 #note all our filenames for printing
+        infilecontents = pd.concat(                             #Combine all the data, but only the columns we need.
             [infilecontents,pd.read_csv(arg,delim_whitespace=True,usecols=['EV','TS','P','Type','E1','D3','time1','nCap'])]
             )
 
 print(infilecontents)
-#infilecontents = [x.replace('\n','').rstrip() for x in infilecontents] #Remove newlines at the end of each list entry.
 
 #Add some header info before I modify the data. ------------------------
 print("# Un-pruned file(s):", *infilenames, sep = ' ')
@@ -36,11 +35,10 @@ print("# Number of entries before pruning:",len(infilecontents.index))
 outfilecontents = pd.DataFrame() #initialize this so I can add to it.
 for value in infilecontents.query('nCap == 1')['EV']:
     outfilecontents = pd.concat([outfilecontents,                                                          #append something to the output contents.
-        infilecontents[infilecontents.query('nCap == 1 and EV == @value').index[0]:].query('EV == @value')  #Include the whole event, starting from the initial capture
+        infilecontents[infilecontents.query('nCap == 1 and EV == @value').index[0]:].query('EV == @value') #Include the whole event, starting from the initial capture
     ])
 
 # pruning done, back to printing. --------------------------------------
 print("# Capture events found:",len(infilecontents.query('nCap == 1').index)) #Print this before I change it
 print("# Total lines output:",len(outfilecontents.index))
-#print(open(sys.argv[1]).readlines()[1].replace('\n',''),'chainkey') #include the table header once.
 outfilecontents.to_csv(sys.argv[-1])
