f8abd6df4712174db280bc90acb4c0918288f347
Kitty <kathryn.harris@ucdenver.edu>
2022-01-29 18:29:03 -0700

    Finish first complete draft of prune.py.

--- python/prune.py
+++ python/prune.py
@@ -31,14 +31,16 @@ print(infilecontents)
 print("# Un-pruned file(s):", *infilenames, sep = ' ')
 print("# Number of entries before pruning:",len(infilecontents.index))
 
-#Here's the bulk of the pruning process. -------------------------------
+#Here's the main the pruning process. ----------------------------------
 
-outfilecontents = infilecontents.query('nCap==1') #Find all the initial captures.
-print("# Capture events found:",len(outfilecontents.index)) #Print this before I change it
-
-#More will go here once I figure out how to get it working.
+outfilecontents = pd.DataFrame() #initialize this so I can add to it.
+for value in infilecontents.query('nCap == 1')['EV']:
+    outfilecontents = pd.concat([outfilecontents,                                                          #append something to the output contents.
+        infilecontents[infilecontents.query('nCap == 1 and EV == @value').index[0]:].query('EV == @value')  #Include the whole event, starting from the initial capture
+    ])
 
 # pruning done, back to printing. --------------------------------------
-print("# Total events output:",len(outfilecontents.index))
-print(open(sys.argv[1]).readlines()[1].replace('\n',''),'chainkey') #include the table header once.
+print("# Capture events found:",len(infilecontents.query('nCap == 1').index)) #Print this before I change it
+print("# Total lines output:",len(outfilecontents.index))
+#print(open(sys.argv[1]).readlines()[1].replace('\n',''),'chainkey') #include the table header once.
 outfilecontents.to_csv(sys.argv[-1])
